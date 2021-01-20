6b79c295d942608b39969b63579a15a069291e96
Sugeily <sugeilymg@gmail.com>
2021-01-19 23:23:50 -0600

    Commit 2

--- PyBank/analysis/Financial_Analysis.py
+++ PyBank/analysis/Financial_Analysis.py
@@ -1,16 +1,21 @@
 import os
 import csv
-
+Total_Months=[]
+Total=[]
+Change=[]
+Greastest_Increase=[]
+Greastest_Decrease=[]
+count=0
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
-
-    csvreader = csv.reader(csvfile,delimiter=',')
-    print(csvreader)
-
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
+    
+    csvreader = csv.reader(csvfile) 
+    
     for row in csvreader:
-        print(row)
-
+        
+        count=count+1
+        
+    Total_Months.append(count-1)
+    print(Total_Months) 
+       
